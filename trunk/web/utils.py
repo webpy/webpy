8f63a0521c4657b0353f3ef88a72bcd9e8fcbdf0
aaronsw <>
2006-07-17 22:00:34 +0000

    to36

--- trunk/web/utils.py
+++ trunk/web/utils.py
@@ -18,6 +18,7 @@ __all__ = [
   "tryall",
   "ThreadedDict",
   "autoassign",
+  "to36",
   "safemarkdown"
 ]
 
@@ -676,6 +677,33 @@ def autoassign(self, locals):
         if key == 'self': 
             continue
         setattr(self, key, value)
+
+def to36(q):
+    """
+    Converts an integer to base 36 (a useful scheme for human-sayable IDs).
+    
+        >>> to36(35)
+        'z'
+        >>> to36(119292)
+        '2k1o'
+        >>> int(to36(939387374), 36)
+        939387374
+        >>> to36(0)
+        '0'
+        >>> to36(-393)
+        Traceback (most recent call last):
+            ... 
+        ValueError: must supply a positive integer
+    
+    """
+    if q < 0: raise ValueError, "must supply a positive integer"
+    letters = "0123456789abcdefghijklmnopqrstuvwxyz"
+    converted = []
+    while q != 0:
+        q, r = divmod(q, 36)
+        converted.insert(0, letters[r])
+    return "".join(converted) or '0'
+
 
 r_url = re_compile('(?<!\()(http://(\S+))')
 def safemarkdown(text):

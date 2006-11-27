7bc9cfa316fcd4308649e41c3dead1e3aa47daf4
aaronsw <>
2006-11-27 23:42:46 +0000

    setup.py file (tx Tim Riley)

---
+++ trunk/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='web.py',
+      version='0.2',
+      description='web.py: makes web apps',
+      author='Aaron Swartz',
+      author_email='me@aaronsw.com',
+      url=' http://webpy.org/',
+      packages=['web'],
+      long_description="Think about the ideal way to write a web app. Write the code to make it happen.",
+      license="Public domain",
+      platforms=["any"],
+     )

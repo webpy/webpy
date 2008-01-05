74146b44df54a1724617a31110a1cfc8371f4e12
Anand <anandology@gmail.com>
2008-01-05 16:56:31 +0530

    more tests

--- test/db.py
+++ test/db.py
@@ -6,9 +6,8 @@ class DBTest(webtest.TestCase):
     dbname = 'postgres'
     
     def setUp(self):
-        web.config._hasPooling = False
         webtest.setup_database(self.dbname)
-        web.query("CREATE TABLE person (name text unique, email text)")
+        web.query("CREATE TABLE person (name text, email text)")
 
     def tearDown(self):
         # there might be some error with the current connection, delete from a new connection
@@ -27,11 +26,65 @@ class DBTest(webtest.TestCase):
             pass
         web.select('person')
 
+    def assertRows(self, n):
+        result = web.select('person')
+        self.assertEquals(len(list(result)), n)
+
+    def testCommit(self):
+        web.transact()
+        web.insert('person', False, name='user1')
+        web.commit()
+
+        web.transact()
+        web.insert('person', False, name='user2')
+        web.insert('person', False, name='user3')
+        web.commit()
+        self.assertRows(3)
+
+    def testRollback(self):
+        web.transact()
+        web.insert('person', False, name='user1')
+        web.insert('person', False, name='user2')
+        web.insert('person', False, name='user3')
+        web.rollback()
+        self.assertRows(0)
+
+    def testNestedTransactions(self):
+        web.transact()
+        web.insert('person', False, name='user1')
+        self.assertRows(1)   
+
+        web.transact()
+        web.insert('person', False, name='user2')
+        self.assertRows(2)
+        web.rollback()
+        self.assertRows(1)
+
+        web.transact()
+        web.insert('person', False, name='user3')
+        web.commit()
+
+        self.assertRows(2)
+        web.commit()
+        
+
+
+
 class SqliteTest(DBTest):
     dbname = "sqlite"
+
+    def testNestedTransactions(self):
+        # nested transactions are not supported in sqlite
+        pass
     
 class MySQLTest(DBTest):
     dbname = "mysql"
+
+    def setUp(self):
+        web.config._hasPooling = False
+        webtest.setup_database(self.dbname)
+        # In mysql, transactions are supported only with INNODB engine.
+        web.query("CREATE TABLE person (name text, email text) ENGINE InnoDB")
 
 if __name__ == '__main__':
     webtest.main()
--- test/webtest.py
+++ test/webtest.py
@@ -1,82 +1,13 @@
 """webtest: test utilities.
 """
 import unittest
+from unittest import TestCase
 import sys, os
 
 # adding current directory to path to make sure local copy of web module is used.
 sys.path.insert(0, '.')
 
 import web
-
-
-class TestCase(unittest.TestCase):
-    def setUpAll(self):
-        pass
-        
-    def tearDownAll(self):
-        pass
-        
-    def shortDescription(self):
-        """overridden to not return docstrings"""
-        return None
-        
-class TestSuite(unittest.TestSuite):
-    """A TestSuite with once per TestCase setUpAll() and tearDownAll().
-    Adopted from test/testlib/testing.py file in SQLAlchemy test suite. 
-    """
-
-    def __init__(self, tests=()):
-        if len(tests) >0 and isinstance(tests[0], TestCase):
-            self._initTest = tests[0]
-        else:
-            self._initTest = None
-        unittest.TestSuite.__init__(self, tests)
-
-    def do_run(self, result):
-        # nice job unittest !  you switched __call__ and run() between py2.3
-        # and 2.4 thereby making straight subclassing impossible !
-        for test in self._tests:
-            if result.shouldStop:
-                break
-            test(result)
-        return result
-
-    def run(self, result):
-        return self(result)
-
-    def __call__(self, result):
-        try:
-            if self._initTest is not None:
-                self._initTest.setUpAll()
-        except:
-            # skip tests if global setup fails
-            ex = self.__exc_info()
-            for test in self._tests:
-                result.addError(test, ex)
-            return False
-        try:
-            return self.do_run(result)
-        finally:
-            try:
-                if self._initTest is not None:
-                    self._initTest.tearDownAll()
-            except:
-                result.addError(self._initTest, self.__exc_info())
-                pass
-
-    def __exc_info(self):
-        """Return a version of sys.exc_info() with the traceback frame
-           minimised; usually the top level of the traceback frame is not
-           needed.
-           ripped off out of unittest module since its double __
-        """
-        exctype, excvalue, tb = sys.exc_info()
-        if sys.platform[:4] == 'java': ## tracebacks look different in Jython
-            return (exctype, excvalue, tb)
-        return (exctype, excvalue, tb)
-
-# monkeypatch
-unittest.TestLoader.suiteClass = TestSuite
 
 def runTests(suite):
     runner = unittest.TextTestRunner()
@@ -123,5 +54,5 @@ def setup_database(dbname):
     else:
         web.config.db_parameters = dict(dbn=dbname, db='webpy', user='scott', pw='tiger')
         
-    web.config.db_printing=True
+    #web.config.db_printing=True
     web.load()

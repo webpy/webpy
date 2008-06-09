c6c8a392febc1f14880b6965509d0c53eb6eade9
Anand <anand@bodhi>
2008-06-09 17:59:19 +0530

    added web.contrib to setup.py and changed version to 0.3

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@
 from distutils.core import setup
 
 setup(name='web.py',
-      version='0.22',
+      version='0.3',
       description='web.py: makes web apps',
       author='Aaron Swartz',
       author_email='me@aaronsw.com',
       url=' http://webpy.org/',
-      packages=['web', 'web.wsgiserver'],
+      packages=['web', 'web.wsgiserver', 'web.contrib'],
       long_description="Think about the ideal way to write a web app. Write the code to make it happen.",
       license="Public domain",
       platforms=["any"],

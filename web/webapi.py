4020f6e430f9275e57fefd3a95cb33d15a8ea2d2
Anand Chitipothu <anandology@gmail.com>
2008-10-25 11:38:45 +0530

    fixed unicode error in setcookie

--- web/webapi.py
+++ web/webapi.py
@@ -235,7 +235,7 @@ def cookies(*requireds, **defaults):
     try:
         d = storify(cookie, *requireds, **defaults)
         for k, v in d.items():
-            d[k] = urllib.unquote(v)
+            d[k] = urllib.unquote(utf8(v))
         return d
     except KeyError:
         badrequest()

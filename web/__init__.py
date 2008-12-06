b0ef29382ff201bfa2b03212ff4a222b58017b9d
Anand Chitipothu <anandology@gmail.com>
2008-12-06 07:46:01 +0530

    cleanup

--- web/__init__.py
+++ web/__init__.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
+"""web.py: makes web apps (http://webpy.org)"""
+
 from __future__ import generators
 
-"""web.py: makes web apps (http://webpy.org)"""
 __version__ = "0.3"
-__revision__ = "$Rev$"
 __author__ = "Aaron Swartz <me@aaronsw.com>"
 __license__ = "public domain"
 __contributors__ = "see http://webpy.org/changes"
-
-# todo:
-#   - some sort of accounts system
 
 import utils, db, net, wsgi, http, webapi, httpserver, debugerror
 import template, form
@@ -29,29 +26,4 @@ try:
     import webopenid as openid
 except ImportError:
     pass # requires openid module
-
-def main():
-    import doctest
-    
-    doctest.testmod(utils)
-    doctest.testmod(db)
-    doctest.testmod(net)
-    doctest.testmod(wsgi)
-    doctest.testmod(http)
-    doctest.testmod(webapi)
-    
-    template.test()
-    
-    import sys
-    urls = ('/web.py', 'source')
-    class source:
-        def GET(self):
-            header('Content-Type', 'text/python')
-            return open(sys.argv[0]).read()
-    
-    if listget(sys.argv, 1) != 'test':
-        app = application(urls, locals())
-        app.run()
-
-if __name__ == "__main__": main()
 

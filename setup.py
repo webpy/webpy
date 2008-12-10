989b22ad891741a843d572f0cb34c601ffbb0f89
Anand Chitipothu <anandology@gmail.com>
2008-12-10 17:37:06 +0530

    change version to 0.31

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 
 setup(name='web.py',
-      version='0.3',
+      version='0.31',
       description='web.py: makes web apps',
       author='Aaron Swartz',
       author_email='me@aaronsw.com',

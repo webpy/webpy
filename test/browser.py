ee08318cda84632407bc2cf633ef432f610acb62
Anand Chitipothu <anandology@gmail.com>
2008-12-10 12:45:11 +0530

    test redirects

--- test/browser.py
+++ test/browser.py
@@ -2,10 +2,22 @@ import webtest
 import web
 
 urls = (
+    "/", "index",
+    "/hello/(.*)", "hello",
     "/cookie", "cookie",
     "/setcookie", "setcookie",
+    "/redirect", "redirect",
 )
 app = web.application(urls, globals())
+
+class index:
+    def GET(self):
+        return "welcome"
+
+class hello:
+    def GET(self, name):
+        name = name or 'world'
+        return "hello, " + name + '!'
 
 class cookie:
     def GET(self):
@@ -18,6 +30,11 @@ class setcookie:
             web.setcookie(k, v)
         return "done"
 
+class redirect:
+    def GET(self):
+        i = web.input(url='/')
+        raise web.seeother(i.url)
+
 class BrowserTest(webtest.TestCase):
     def testCookies(self):
         b = app.browser()
@@ -29,6 +46,19 @@ class BrowserTest(webtest.TestCase):
         b = app.browser()
         b.open('http://0.0.0.0/notfound')
         self.assertEquals(b.status, 404)
+
+    def testRedirect(self):
+        b = app.browser()
+
+        b.open('http://0.0.0.0:8080/redirect')
+        self.assertEquals(b.url, 'http://0.0.0.0:8080/')
+        b.open('http://0.0.0.0:8080/redirect?url=/hello/foo')
+        self.assertEquals(b.url, 'http://0.0.0.0:8080/hello/foo')
+
+        b.open('https://0.0.0.0:8080/redirect')
+        self.assertEquals(b.url, 'https://0.0.0.0:8080/')
+        b.open('https://0.0.0.0:8080/redirect?url=/hello/foo')
+        self.assertEquals(b.url, 'https://0.0.0.0:8080/hello/foo')
 
 if __name__ == "__main__":
     webtest.main()

4db7f3999b3e66a3f7cf3062973a7b9e582174a8
Anand Chitipothu <anandology@gmail.com>
2009-01-15 10:40:46 +0530

    make web.commify work with decimals (Bug#317204)

--- web/utils.py
+++ web/utils.py
@@ -636,17 +636,34 @@ def commify(n):
         '1,234'
         >>> commify(1234567890)
         '1,234,567,890'
+        >>> commify(123.0)
+        '123.0'
+        >>> commify(1234.5)
+        '1,234.5'
+        >>> commify(1234.56789)
+        '1,234.56789'
+        >>> commify('%.2f' % 1234.5)
+        '1,234.50'
         >>> commify(None)
         >>>
-    
+
     """
     if n is None: return None
+    n = str(n)
+    if '.' in n:
+        dollars, cents = n.split('.')
+    else:
+        dollars, cents = n, None
+
     r = []
-    for i, c in enumerate(reversed(str(n))):
+    for i, c in enumerate(reversed(str(dollars))):
         if i and (not (i % 3)):
             r.insert(0, ',')
         r.insert(0, c)
-    return ''.join(r)
+    out = ''.join(r)
+    if cents:
+        out += '.' + cents
+    return out
 
 def dateify(datestring):
     """

2bf214e14e6cbb523eca138cfda621b78798f74c
Anand Chitipothu <anandology@gmail.com>
2009-05-08 15:27:30 +0530

    use ident authentication for postgres test db

--- test/webtest.py
+++ test/webtest.py
@@ -1,6 +1,6 @@
 """webtest: test utilities.
 """
-import sys
+import sys, os
 
 # adding current directory to path to make sure local modules can be imported
 sys.path.insert(0, '.')
@@ -10,6 +10,9 @@ from web.test import *
 def setup_database(dbname, driver=None, pooling=False):
     if dbname == 'sqlite':
         db = web.database(dbn=dbname, db='webpy.db', pooling=pooling, driver=driver)
+    elif dbname == 'postgres':
+        user = os.getenv('USER')
+        db = web.database(dbn=dbname, db='webpy', user=user, pw='', pooling=pooling, driver=driver)
     else:
         db = web.database(dbn=dbname, db='webpy', user='scott', pw='tiger', pooling=pooling, driver=driver)
 

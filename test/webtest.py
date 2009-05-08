d0e2dc276821c239949270df06f3b0e1f08a1ce9
Anand Chitipothu <anandology@gmail.com>
2009-05-08 10:49:36 +0530

    forgot to checkin webtest.py

--- test/webtest.py
+++ test/webtest.py
@@ -7,11 +7,11 @@ sys.path.insert(0, '.')
 
 from web.test import *
     
-def setup_database(dbname, pooling=False):
+def setup_database(dbname, driver=None, pooling=False):
     if dbname == 'sqlite':
-        db = web.database(dbn=dbname, db='webpy.db', pooling=pooling)
+        db = web.database(dbn=dbname, db='webpy.db', pooling=pooling, driver=driver)
     else:
-        db = web.database(dbn=dbname, db='webpy', user='scott', pw='tiger', pooling=pooling)
+        db = web.database(dbn=dbname, db='webpy', user='scott', pw='tiger', pooling=pooling, driver=driver)
 
     db.printing = '-v' in sys.argv
     return db

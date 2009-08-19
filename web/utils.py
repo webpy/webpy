e20a708ea24c13485434432597b967afc001fd27
Anand Chitipothu <anandology@gmail.com>
2009-08-19 17:03:18 +0530

    fix a bug in web.group

--- web/utils.py
+++ web/utils.py
@@ -425,11 +425,21 @@ def group(seq, size):
 
         >>> list(group([1,2,3,4], 2))
         [[1, 2], [3, 4]]
+        >>> list(group([1,2,3,4,5], 2))
+        [[1, 2], [3, 4], [5]]
     """
+    def take(seq, n):
+        for i in xrange(n):
+            yield seq.next()
+
     if not hasattr(seq, 'next'):  
         seq = iter(seq)
     while True: 
-        yield [seq.next() for i in xrange(size)]
+        x = list(take(seq, size))
+        if x:
+            yield x
+        else:
+            break
 
 def uniq(seq):
    """

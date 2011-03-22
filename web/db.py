b5d2c796720e240c15dffc1e322ee3b9b4e1c37b
Nimrod S. Kerrett <dod@goleshet.com>
2011-03-23 04:42:53 +0700

    No SQL debug logs unless web.config.debug_sql is set
    Trivial, but helpful.

--- web/db.py
+++ web/db.py
@@ -464,7 +464,7 @@ class DB:
         
         self._ctx = threadeddict()
         # flag to enable/disable printing queries
-        self.printing = config.get('debug', False)
+        self.printing = config.get('debug_sql', False) ###@@@ was 'debug'. was irritating :)
         self.supports_multiple_insert = False
         
         try:

00334f6295b81a4de5bea5756569d1dffaad7f4c
Michael Gorven <michael@gorven.za.net>
2011-07-11 14:03:59 +0200

    Handle XHTML.

--- web/session.py
+++ web/session.py
@@ -34,7 +34,7 @@ web.config.session_parameters = utils.storage({
     'secret_key': 'fLjUfxqXtfNoIldA0A0J',
     'expired_message': 'Session expired',
     'httponly': True,
-    'secure': False
+    'secure': False,
     'cookieless': False,
 })
 
@@ -157,17 +157,31 @@ class Session(object):
 
         # Only process response if client didn't provide a session cookie
         if not web.cookies().get(cookie_name):
-            doc = html.document_fromstring(str(response))
+            content_type = None
+            for header, value in web.ctx.get('headers', []):
+                if header.lower() == 'content-type':
+                    content_type = value.lower().split(';')[0]
+
+            if content_type == 'text/html':
+                tree = html
+                doc = html.document_fromstring(str(response))
+                ns = ''
+            elif content_type in ('application/xhtml+xml', 'application/xml'):
+                tree = etree
+                doc = etree.fromstring(str(response))
+                ns = '{%s}' % doc.nsmap[None]
+            else:
+                return response
 
             # Add hidden input fields to forms
-            for form in doc.iterfind('.//form'):
+            for form in doc.iterfind('.//%sform' % ns):
                 if 'action' not in form.attrib or self._is_relative(form.attrib['action']):
                     input = etree.Element('input', type='hidden', name=cookie_name, id=cookie_name, value=self.session_id)
                     form.append(input)
 
             # Add query parameters to relative links
             param = cookie_name + '=' + self.session_id
-            for a in doc.iterfind('.//a'):
+            for a in doc.iterfind('.//%sa' % ns):
                 if 'href' in a.attrib and self._is_relative(a.attrib['href']):
                     parts = list(urlsplit(a.attrib['href']))
                     if len(parts[3]) == 0:
@@ -176,7 +190,7 @@ class Session(object):
                         parts[3] += '&' + param
                     a.attrib['href'] = urlunsplit(parts)
                 
-            return html.tostring(doc)
+            return tree.tostring(doc)
         return response
             
     def _setcookie(self, session_id, expires='', **kw):

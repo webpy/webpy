f961ab0ae99286b26409157f1032eb58d432360e
Michael Gorven <michael@gorven.za.net>
2011-07-11 11:24:12 +0200

    Only find template class if we've found the file.

--- web/contrib/template.py
+++ web/contrib/template.py
@@ -137,9 +137,9 @@ class render_tempita:
         for directory in self.directories:
             for extension in self.extensions:
                 filename = os.path.join(directory, name + extension)
-                if not from_template:
-                    from_template = getattr(tempita, self.extensions[extension][0])
                 if os.path.exists(filename):
+                    if not from_template:
+                        from_template = getattr(tempita, self.extensions[extension][0])
                     template = from_template.from_filename(filename, get_template=self._get_template)
                     template.content_type = self.extensions[extension][1]
                     return template

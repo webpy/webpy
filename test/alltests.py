cb8f234a67621667948af93ca8cc7cac0b0c15b7
egbutter <hola@ericbeurre.com>
2014-02-19 19:39:36 -0500

    Improving Templetor security feature (thanks egbutter)
    This commit is a merge of egbutter's Templetor modifications with the
    Py3 port.

--- test/alltests.py
+++ test/alltests.py
@@ -1,7 +1,7 @@
 import webtest
 
 def suite():
-    modules = ["doctests", "db", "application", "session", "wsgi"]
+    modules = ["doctests", "db", "application", "session", "template", "wsgi"]
     return webtest.suite(modules)
     
 if __name__ == "__main__":

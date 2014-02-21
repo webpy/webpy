a57286f09846156f08b4ef8b5f5c1444ec7f80b4
Petr Horacek <p.horacek94@gmail.com>
2014-02-22 00:15:33 +0100

    Auth module - test for auth module added

--- test/alltests.py
+++ test/alltests.py
@@ -1,8 +1,8 @@
 import webtest
 
 def suite():
-    modules = ["doctests", "db", "application", "session"]
+    modules = ["doctests", "db", "application", "session", "auth"]
     return webtest.suite(modules)
-    
+
 if __name__ == "__main__":
     webtest.main()

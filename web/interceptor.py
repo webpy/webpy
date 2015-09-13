dc5f8eafb34bffe90fedbd4598956203eb400a1e
quinn <mail.zhuangkun@gmail.com>
2015-09-13 21:36:07 +0800

    增加注释

--- web/interceptor.py
+++ web/interceptor.py
@@ -1,4 +1,64 @@
-__author__ = 'quinn'
+"""
+Interceptor support
+e.g.
+if __name__ == '__main__':
+    class I(object):
+
+        def before(self, context, name):
+            print "I'm before in I"
+            print context
+            print name
+            context.replace_arg('sex', '2')
+            context["before"] = "hello"
+            return Invocation.SUCCESS
+
+        def after(self, context, name):
+            print "I'm after in I"
+            print context
+            print name
+            context["after"] = "world1"
+
+    class O(object):
+
+        def before(self, context, sex):
+            print "I'm before in O"
+            print context
+            print sex
+            context["before"] = "hello1"
+            return Invocation.SUCCESS
+
+        def after(self, context, name):
+            print "I'm after in O"
+            print context
+            print name
+            context["after"] = "world"
+
+    class C(object):
+
+        def before(self, context, name):
+            print "I'm before in C"
+            print context
+            print name
+            context["before"] = "hello2"
+            context.replace_arg('name', 'quinn')
+            return Invocation.SUCCESS
+
+        def after(self, context, name):
+            print "I'm after in C"
+            print context
+            print name
+            context["after"] = "world2"
+            context.response = "Response has bean replaced."
+
+    class T(object):
+        @interceptor(I, O, C)
+        def test(self, name, sex):
+            print "aaa" + name + sex
+            return "I'm test"
+
+
+    print T().test("bbb", '1')
+"""
 
 import inspect
 
@@ -6,10 +66,23 @@ import inspect
 def enum(**enums):
     return type('Enum', (), enums)
 
+# The return type of Interceptors
 Invocation = enum(SUCCESS=True, FAILED=False, SKIP="SKIP")
 
 
 def interceptor(*classes):
+    """
+    Use decorator to implement interceptor.
+    Every interceptor has two methods, One method is before that execute before the controller;
+    the other method is after that execute after the controller.
+    Each method(before or after) can accept the parameters that accepted by controller,they
+    must define the parameter that named 'context' at first,and framework should pass all these
+    values for you automatically.
+    Also you can change the parameter of the controller with the method 'replace_arg' in context,
+    and you can use 'context.response' to change the result of the controller.
+    :param classes: interceptor classes
+    :return:
+    """
     def decorator(func):
         def _get_params(fun, named):
             params = set(inspect.getargspec(fun)[0])

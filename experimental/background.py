e39b5c40a40fac6ef6f15604e621b87dcd18b761
LiberiFatali <LiberiFatali@users.noreply.github.com>
2015-10-12 16:38:35 +0700

    Update background.py

--- experimental/background.py
+++ experimental/background.py
@@ -1,45 +1,46 @@
 """Helpers functions to run log-running tasks."""
-import threading
-from web import utils
+from web import utils, http
 from web import webapi as web
+import threading
 
 def background(func):
     """A function decorator to run a long-running function as a background thread."""
     def internal(*a, **kw):
-        web.data() # cache it
-        
-        web._context = {}
-        web._context[threading.currentThread()] = utils.storage(web.ctx.copy())
-        tmpctx = web._context[threading.currentThread()]
+	web.data() # cache it
 
-        def newfunc():
-            web._context[threading.currentThread()] = tmpctx
-            func(*a, **kw)
-            myctx = web._context[threading.currentThread()]
-            for k in myctx.keys():
-                if k not in ['status', 'headers', 'output']:
-                    try: del myctx[k]
-                    except KeyError: pass
-        
-        t = threading.Thread(target=newfunc)
-        background.threaddb[id(t)] = t
-        t.start()
-        web.ctx.headers = []
-        return seeother(changequery(_t=id(t)))
+	web._context = {}
+	web._context[threading.currentThread()] = utils.storage(web.ctx.copy())
+	tmpctx = web._context[threading.currentThread()]
+
+	def newfunc():
+	    web._context[threading.currentThread()] = tmpctx
+	    func(*a, **kw)
+	    try:
+		myctx = web._context[threading.currentThread()]
+		for k in myctx.keys():
+		    if k not in ['status', 'headers', 'output']:
+			del myctx[k]
+	    except 
+		KeyError: pass
+
+	t = threading.Thread(target=newfunc)
+	background.threaddb[id(t)] = t
+	t.start()
+	web.ctx.headers = []
+	return web.seeother(http.changequery(_t=id(t)))
     return internal
 background.threaddb = {}
 
 def backgrounder(func):
     def internal(*a, **kw):
-        i = web.input(_method='get')
-        if '_t' in i:
-            try:
-                t = background.threaddb[int(i._t)]
-            except KeyError:
-                return web.notfound()
-            web._context[threading.currentThread()] = web._context[t]
-            return 'Process running background...'
-        else:
-            return func(*a, **kw)
+	i = web.input(_method='get')
+	if '_t' in i:
+	    try:
+		t = background.threaddb[int(i._t)]
+	    except KeyError:
+		return web.notfound()
+	    web._context[threading.currentThread()] = web._context[t]
+	    return 'Process running background...'
+	else:
+	    return func(*a, **kw)
     return internal
-

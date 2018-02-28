25fd477bf0dc824d7886d5ef9aab594f972e3213
Anand Chitipothu <anandology@gmail.com>
2018-02-28 16:13:16 +0530

    Migrated form test cases to py.test.

---
+++ tests/test_form.py
@@ -0,0 +1,7 @@
+from web.form import Form, Textbox
+
+def test_id_escape_issue():
+    f = Form(Textbox("x", id="x <unsafe>"))
+    assert "<unsafe>" not in f.render()
+    assert "<unsafe>" not in f.render_css()
+

7391de4f4d35b742cacdb10070a28c1124636199
cclauss <cclauss@me.com>
2019-09-17 17:33:47 +0200

    Add markdown to setup.py

--- setup.py
+++ setup.py
@@ -20,4 +20,13 @@ setup(
     ),
     license="Public domain",
     platforms=["any"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )

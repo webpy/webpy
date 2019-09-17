034ecb1abf78f4be3f54f6c1437a20cc7cf23c66
cclauss <cclauss@me.com>
2019-09-17 17:26:36 +0200

    Add markdown to setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,11 @@ setup(
     maintainer_email="anandology@gmail.com",
     url="http://webpy.org/",
     packages=["web", "web.contrib"],
-    install_requires=["cheroot"],
-    long_description="Think about the ideal way to write a web app. Write the code to make it happen.",
+    install_requires=["cheroot", "markdown"],
+    long_description=(
+        "Think about the ideal way to write a web app. "
+        "Write the code to make it happen."
+    ),
     license="Public domain",
     platforms=["any"],
 )

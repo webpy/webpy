ccf7c4e3728b93e71d1cd9de4ea127de3e0f817d
Christian Clauss <cclauss@me.com>
2020-03-18 14:39:52 +0100

    setup.py: Add a PyPI trove classifier for Py38
    https://pypi.org/classifiers

--- setup.py
+++ setup.py
@@ -35,5 +35,6 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )

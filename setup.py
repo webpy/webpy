922d412f941e0a835560ca130d499cf190611f13
Christian Clauss <cclauss@me.com>
2020-03-23 14:07:05 +0100

    setup.py: Remove "Programming Language :: Python :: 2.7"

--- setup.py
+++ setup.py
@@ -30,8 +30,6 @@ setup(
     classifiers=[
         "License :: Public Domain",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",

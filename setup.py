cc94596f6f0cd94769fb88888432af6236d36222
Zhang Huangbin <zhb@iredmail.org>
2020-03-23 16:57:28 +0800

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 
@@ -29,9 +29,6 @@ setup(
     platforms=["any"],
     classifiers=[
         "License :: Public Domain",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",

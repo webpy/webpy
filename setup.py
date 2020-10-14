62925f5f977b5e79f8882116a2509c1f582ecf25
Christian Clauss <cclauss@me.com>
2020-10-14 10:28:32 +0200

    setup.py: Add Python 3.9

--- setup.py
+++ setup.py
@@ -32,9 +32,9 @@ setup(
         "License :: Public Domain",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )

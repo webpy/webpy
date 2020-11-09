3f669e4d5a58916003b57ccbfacdc1595c43edca
Christian Clauss <cclauss@me.com>
2020-11-09 09:08:02 +0100

    setup.py: One last release supporting Python 3.5

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "License :: Public Domain",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
